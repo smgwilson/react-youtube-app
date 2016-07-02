8673afb2a9358592d92245f62ba4c283a236f735
Sheri Wilson <swilson@simmonsresearch.com>
2016-07-02 16:11:32 -0700

    initial commit of non-boilerplate project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = "AIzaSyCdNAXXNdENPnkLimHg30OSmzHufzYhzto";
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+//Create a new component. This component should produce some HTML
+
+//Take this component's generated HTML and put it on the page (in the DOM)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'american balinese cats'}, (videos) => {
+        this.setState({ videos });
+        // this.setState ({videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
