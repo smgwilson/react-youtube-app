a7bf62221a1fabb31d5c097226f5164047861e65
Sheri Wilson <swilson@simmonsresearch.com>
2016-07-03 15:00:17 -0700

    adds callback function and styling

--- src/index.js
+++ src/index.js
@@ -18,10 +18,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state= { videos: [] };
+    this.state= {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'american balinese cats'}, (videos) => {
-        this.setState({ videos });
+        this.setState({
+          videos: videos,
+          selectedVideo: videos[0]
+        });
         // this.setState ({videos: videos });
     });
   }
@@ -30,8 +36,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos} />
       </div>
     );
   }
