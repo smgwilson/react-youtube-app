5ff2a199a085e8092517b1f3d800b347e95f67e0
Sheri Wilson <swilson@simmonsresearch.com>
2016-07-03 13:04:17 -0700

    handled null video_detail

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
+
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyCdNAXXNdENPnkLimHg30OSmzHufzYhzto";
 
@@ -28,6 +30,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
