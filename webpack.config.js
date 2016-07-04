363fcf87f52e69e6d8bf3b68eeae477e787e944a
Sheri Wilson <swilson@simmonsresearch.com>
2016-07-04 16:13:09 -0700

    modifies for use on heroku

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,9 @@ module.exports = {
   },
   module: {
     loaders: [{
+      test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
